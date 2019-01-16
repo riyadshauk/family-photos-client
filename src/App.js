19fcb90bb2c73092872e291bd8582c591174c324
Riyad Shauk <riyad.shauk@gmail.com>
2019-01-15 21:50:02 -0800

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -21,22 +21,32 @@ import Public from './Public';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <AuthButton />
-        <LoginForm />
-        <ul>
-          <li>
-            <Link to="/public">Public Page</Link>
-          </li>
-          <li>
-            <Link to="/photofeed">Protected Photo Feed</Link>
-          </li>
-        </ul>
-        <Route path="/public" component={Public} />
-        <PrivateRoute path="/photofeed" component={PhotoFeed} />
-      </div>
-    </Router>
+    <Fragment>
+      <Router>
+        <div>
+          <AuthButton />
+          <LoginForm />
+          <ul>
+            <li>
+              <Link to="/public">Public Page</Link>
+            </li>
+            <li>
+              <Link to="/photofeed">Protected Photo Feed</Link>
+            </li>
+          </ul>
+          <Route path="/public" component={Public} />
+          <PrivateRoute path="/photofeed" component={PhotoFeed} />
+        </div>
+      </Router>
+      <footer>
+        <div>
+          View the <a href="https://github.com/riyadshauk/family-photos-server">server-side</a> and <a href="https://github.com/riyadshauk/family-photos-client">client-side</a> code for this photo-sharing app on GitHub.
+        </div>
+        <div>
+          Copyright © 2019, Riyad Shauk
+        </div>
+      </footer>
+    </Fragment>
   );
 };
 
